29e0dbde3df74c0eb24d20a6316834dd58ce9190
nasm <40870683+n4sm@users.noreply.github.com>
2019-10-02 17:16:44 +0200

    Update stub_not_pie.asm

--- packer@git/stub/stub_not_pie.asm
+++ packer@git/stub/stub_not_pie.asm
@@ -1,3 +1,18 @@
+; * This file is part of the nasm distribution (https://github.com/n4sm/m0dern_p4cker/).
+; * Copyright (c) 2019 nasm.
+; * 
+; * This program is free software: you can redistribute it and/or modify  
+; * it under the terms of the GNU General Public License as published by  
+; * the Free Software Foundation, version 3.
+; *
+; * This program is distributed in the hope that it will be useful, but 
+; * WITHOUT ANY WARRANTY; without even the implied warranty of 
+; * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+; * General Public License for more details.
+; *
+; * You should have received a copy of the GNU General Public License 
+; * along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 section .text
     global _start
 
